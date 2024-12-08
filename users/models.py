21535d64da5b2983279e353e6a91b503489965a8
Jacob <jacobmeide@gmail.com>
2024-12-07 22:26:05 -0600

    Custom User Model and SIngle route

---
+++ users/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+
+class CustomUser(AbstractUser):
+    username = models.CharField(max_length=100, unique=True)
+    email = models.EmailField(max_length=100, unique=True)
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    date_joined = models.DateTimeField(auto_now_add=True)
+    last_login = models.DateTimeField(auto_now=True)
+    is_staff = models.BooleanField(default=False)
